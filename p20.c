f352f6a3bd7f3bed88e30fcc023d5da3203c1049
Priyanuj Bora <while.alive.program@gmail.com>
2024-02-08 16:52:46 +0530

    print histogram of the lengths of words in input(the histogram will be a vertical one)

---
+++ p20.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+
+int main(void) {
+	int c, len = 0, idx = 0;
+
+	int length[10];  // let's assume that there are going to be 10 words or less than 10 words in a string
+
+	while((c = getchar()) != EOF) {
+		if((c == ' ' || c == '\t' || c == '\n') && len > 0) {
+			length[idx++] = len;
+			len = 0;
+		}
+
+		else if(c != ' ' && c != '\t' && c != '\n')
+			++len;
+	}
+
+	puts("Histogram:");
+
+	// vertical histogram
+
+	return 0;
+}
