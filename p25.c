5e5e091a9b4fdb24d313bb5040f17712c26ddaee
Priyanuj Bora <while.alive.program@gmail.com>
2024-02-09 20:05:07 +0530

    print the correct length of arbritarily long input input lines, and as much as possible of the text

--- p25.c
+++ p25.c
@@ -19,7 +19,7 @@ int main(void) {
 	}
 
 	printf("Longest string: %s\n", longest);
-	printf("\nLength: %d\n", max);
+	printf("Length: %d\n", max);
 
 	return 0;
 }
@@ -28,12 +28,43 @@ int getLine(char line[], int maxline) {
 
 	int c, idx = 0;
 
-	while(((c = getchar()) != EOF) && (c != '\n') && (idx != maxline - 1)) {
+	while(((c = getchar()) != EOF) && (c != '\n') && (idx != (maxline - 1))) {
 		line[idx++] = c;
 	}
+
+	if(idx == (maxline - 1)) {
+		line[idx] = '\0';
+		line[idx - 1] = '\n';
+
+		for(int i = idx - 1; i > idx - 3; i--)
+			line[i] = '.';
+		
+		if(c == '\n') {
+			idx++;
+		}
+
+		else {
+			while((c = getchar()) != '\n')
+				idx++;
+		}
+
+		return idx;
+	}
+
+	else if(c == '\n')
+		line[idx++] = c;
+
+	line[idx] = '\0';
+
+	return idx;
 }
 
 void copy(char to[], char from[]) {
-	for(int i = 0; from[i] != '\0'; i++)
+	int idx;
+	for(int i = 0; from[i] != '\n'; i++) {
+		idx = i;
 		to[i] = from[i];
+	}
+
+	to[idx + 1] = '\0';
 }
