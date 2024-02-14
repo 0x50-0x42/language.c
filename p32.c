62758225ebd84bf50ce998129cf70dccc41b4768
Priyanuj Bora <while.alive.program@gmail.com>
2024-02-14 10:49:51 +0530

    incomplete

--- p32.c
+++ p32.c
@@ -1,27 +1,20 @@
 #include<stdio.h>
 
-#define LENGTH 1000
-
-#define COL 100  // after 100 columns of input, the program will break a line!
-
-int getLine(char[]);
+#define NEWLINE '\n'
+#define COL 150
 
 int main(void) {
-	char line[LENGTH];
+	int chars = 0, c;
 
-	int len;
+	while((c = getchar()) != EOF) {
 
-	len = getLine(line);
+		if(chars == COL) {
+		}
+
+		chars++;
+	}
+
+	printf("Characters entered: %d\n", chars);
+
 	return 0;
-}
-
-int getLine(char line[]) {
-	int c, idx = 0;
-
-	while((c = getchar()) != EOF && c != '\n' && idx != LENGTH - 1) {
-		line[idx++] = c;
-	}
-
-	if(idx == LENGTH - 1) {
-	}
 }
