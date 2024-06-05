ea7b3811df7d5138bc4454b8f210202dbd654164
Priyanuj Bora <while.alive.program@gmail.com>
2024-06-05 09:46:17 +0530

    p45.c

---
+++ p45.c
@@ -0,0 +1,12 @@
+/*
+	write the function any(s1, s2), which returns the first location in the string s1 where any character from the string s2 occurs,
+	or -1 if s1 contains no characters from s2.
+*/
+
+#include<stdio.h>
+
+int any(char[], char[]);
+
+int main(void) {
+	return 0;
+}
