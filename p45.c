8c1bd8c46f00e43d646073d7264ea02ee32c0d57
Priyanuj Bora <while.alive.program@gmail.com>
2024-06-07 06:31:03 +0530

    K&R exercise 2.5

--- p45.c
+++ p45.c
@@ -8,5 +8,28 @@
 int any(char[], char[]);
 
 int main(void) {
+
+	char s1[1000], s2[1000];
+
+	printf("enter first string: ");
+	scanf("%[^\n]%*c", s1);
+
+	printf("enter second string: ");
+	scanf("%[^\n]%*c", s2);
+
+	int location = any(s1, s2);
+
+	printf("Location: %d\n", location);
+
 	return 0;
+}
+
+int any(char s1[], char s2[]) {
+	for(int i = 0; s1[i] != '\0'; i++) {
+		for(int j = 0; s2[j] != '\0'; j++)
+			if(s1[i] == s2[j])
+				return i;
+	}
+
+	return -1;
 }
