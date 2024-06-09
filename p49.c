8387448bc766c6715f47783a303878c11a98d915
Priyanuj Bora <while.alive.program@gmail.com>
2024-06-09 20:30:40 +0530

    incomplete

---
+++ p49.c
@@ -0,0 +1,25 @@
+/*
+	write a function rightrot(x, n) that returns the value of the integer x rotated to the right by n bit positions;
+*/
+
+#include<stdio.h>
+
+unsigned rightrot(unsigned, int);
+
+int main(void) {
+
+	unsigned x, result;
+	int n;
+
+	x = 170;
+	n = 3;
+	return 0;
+}
+
+/*
+	n = 3
+	~~~~~
+
+	x = 170
+	00000000 00000000 00000000 10101010
+*/
