aef5804ef9f97a626bc92c7649ae665b7e0e4b20
Priyanuj Bora <while.alive.program@gmail.com>
2024-06-10 06:52:02 +0530

    K&R exercise 2.8

--- p49.c
+++ p49.c
@@ -3,6 +3,7 @@
 */
 
 #include<stdio.h>
+#include<limits.h>
 
 unsigned rightrot(unsigned, int);
 
@@ -11,15 +12,48 @@ int main(void) {
 	unsigned x, result;
 	int n;
 
-	x = 170;
-	n = 3;
+	x = 19;
+	n = 2;
+
+	result = rightrot(x, n);
+
+	printf("Result: %u\n", result);
+
 	return 0;
 }
 
+unsigned rightrot(unsigned x, int n) {
+
+	int d = 8; // assuming x to be an 8-bit number
+
+	return ((x & ~(~0 << n)) << (d-n)) | (x >> n);
+}
+
 /*
-	n = 3
+	n = 2, assuming things for 8-bit numbers, that is, d = 8
 	~~~~~
 
-	x = 170
-	00000000 00000000 00000000 10101010
+	x = 19
+	00000000 00000000 00000000 00010011
+
+	~0 << n
+	11111111 11111111 11111111 11111100
+
+	~(~0 << n)
+	00000000 00000000 00000000 00000011
+
+	x & ~(~0 << n)
+	00000000 00000000 00000000 00000011
+
+	(x & ~(~0 << n)) << (d-n)
+	00000000 00000000 00000000 11000000
+
+	x >> n
+	00000000 00000000 00000000 00000100
+
+	(x >> n) | ((x & ~(~0 << n)) << (d-n))
+	00000000 00000000 00000000 11000100
+
+	CHECK OUT THIS VIDEO:
+	https://youtu.be/m_08FbT0_WY?si=-ufDpjDOhPn6_6k0
 */
