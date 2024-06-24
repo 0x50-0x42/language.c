63ad0de087041de6fb2df0bf3330622bfb5af7ac
Priyanuj Bora <while.alive.program@gmail.com>
2024-06-24 19:47:07 +0530

    incomplete

--- p54.c
+++ p54.c
@@ -18,22 +18,45 @@ int main(void) {
 	scanf("%d", &el);
 
 	int idx = binSearch(el, arr, 7);
+
+	if(idx != -1)
+		printf("binSearch()\n%d is at index %d\n", el, idx);
+	else
+		printf("binSearch()\n%d is not in the array\n", el);
 	idx = binSearch2(el, arr, 7);
 
 	if(idx != -1)
-		printf("%d is at index %d\n", el, idx);
+		printf("binSearch2()\n%d is at index %d\n", el, idx);
 	else
-		printf("%d is not in the array\n", el);
+		printf("binSearch2()\n%d is not in the array\n", el);
 
 	return 0;
 }
 
 int binSearch(int x, int v[], int n) {
-
 	int low = 0;
 	int high = n - 1;
 
-	int idx;
+	int mid;
+
+	while(low <= high) {
+		mid = (low + high) / 2; // getting the middle element
+		
+		if(x < v[mid]) // if target is less than middle element of the array
+			high = mid - 1; // concentrate on the lower-half of the array
+		else if(x > v[mid]) // if target is greater than middle element of the array
+			low = mid + 1; // concentrate on the upper-half of the array
+		else
+			return mid; // return the index
+	}
+
+	return -1; // if the element if not found
+}
+
+int binSearch2(int x, int v[], int n) {
+
+	int low = 0;
+	int high = n - 1;
 
 	int mid = (low + high) / 2;
 
