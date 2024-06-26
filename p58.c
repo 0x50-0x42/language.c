6ebb0ce249d31e4a6e68cf61712f62856f22da4f
Priyanuj Bora <while.alive.program@gmail.com>
2024-06-26 20:42:36 +0530

    K&R exercise 3.3

--- p58.c
+++ p58.c
@@ -22,14 +22,80 @@ int main(void) {
 	while((c = getchar()) != 27) // take input until user presses ESC key
 		s1[idx++] = c;
 
+	expand(s1, s2);
+
+	printf("Expanded string is: %s\n", s2);
+
 	return 0;
 }
 
 void expand(char s1[], char s2[]) {
-	int i;
+	int i, j = 0, ch;
 
 	for(i = 0; isspace(s1[i]); i++); // skip whitespaces(if any) before s1
 
 	for( ; s1[i] != '\0'; i++) {
+		if((s1[i] >= 'a' && s1[i] <= 'z') || (s1[i] >= 'A' && s1[i] <= 'Z')) {
+			if(s1[i + 1] != '\0') {
+				switch(s1[i + 1]) {
+					case '-':
+						if(s1[i + 2]) {
+							ch = s1[i];
+							while(ch <= s1[i + 2])
+								s2[j++] = ch++;
+							i += 2;
+						}
+
+						else
+							s2[j++] = s1[i];
+
+						break;
+
+					case '*': case '/': case '+': case '\\':
+						s2[j++] = s1[i];
+						break;
+
+					default:
+						s2[j++] = s1[i];
+				}
+			}
+
+			else
+				s2[j++] = s1[i];
+		}
+
+		else if(s1[i] >= '0' && s1[i] <= '9') {
+			if(s1[i + 1] != '\0') {
+				switch(s1[i + 1]) {
+					case '-':
+						if(s1[i + 2]) {
+							ch = s1[i];
+							while(ch <= s1[i + 2])
+								s2[j++] = ch++;
+							i += 2;
+						}
+
+						else
+							s2[j++] = s1[i];
+
+						break;
+
+					case '*': case '/': case '+': case '\\':
+						s2[j++] = s1[i];
+						break;
+
+					default:
+						s2[j++] = s1[i];
+				}
+			}
+
+			else
+				s2[j++] = s1[i];
+		}
+
+		else if(s1[i] == '-')
+			s2[j++] = s1[i];
+		else
+			s2[j++] = s1[i];
 	}
 }
