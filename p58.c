a8f241b568545b05f84b41f82025fe81e725e526
Priyanuj Bora <while.alive.program@gmail.com>
2024-06-26 09:11:38 +0530

    incomplete

--- p58.c
+++ p58.c
@@ -2,11 +2,12 @@
 	write a function expand(s1, s2) that expands shorthand notations
 	like a-z in the string s1 into the equivalent complete list abc...xyz
 	in s2; allow for letters of either case and digits, and be prepared
-	to handle cases like a-b-c and a-z0-9 and -a-z;
-	arrange that a leading or trailing - is taken literally;
+	to handle cases like a-b-c and a-z0-9 and -a-z arrange that a leading
+	or trailing - is taken literally;
 */
 
 #include<stdio.h>
+#include<ctype.h>
 
 #define MAXLEN 1000
 
@@ -22,4 +23,13 @@ int main(void) {
 		s1[idx++] = c;
 
 	return 0;
+}
+
+void expand(char s1[], char s2[]) {
+	int i;
+
+	for(i = 0; isspace(s1[i]); i++); // skip whitespaces(if any) before s1
+
+	for( ; s1[i] != '\0'; i++) {
+	}
 }
