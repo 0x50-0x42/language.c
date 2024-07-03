a421b6a02754486692a54a6574c355d93d1bf207
Priyanuj Bora <while.alive.program@gmail.com>
2024-07-03 07:40:21 +0530

    resolving the issue in p65.c

---
+++ p67.c
@@ -0,0 +1,47 @@
+// A solution to p65.c
+
+#include<stdio.h>
+
+
+int main(void) {
+
+	int i = 0;
+	while(i <= 10) {
+		i++;
+		switch(i) {
+			case 1:
+				puts("one");
+				continue;
+			case 2:
+				puts("two");
+				continue;
+			case 3:
+				puts("three");
+				continue;
+			case 4:
+				puts("four");
+				continue;
+			case 5:
+				puts("five");
+				continue;
+			case 6:
+				puts("six");
+				continue;
+			case 7:
+				puts("seven");
+				continue;
+			case 8:
+				puts("eight");
+				continue;
+			case 9:
+				puts("nine");
+				continue;
+			case 10:
+				puts("ten");
+				continue;
+		}
+
+	} // an infinite loop which only displays "one"
+
+	return 0;
+}
