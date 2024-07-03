d452db491ad2a9f05ba244d777e693bf9f3ba48d
Priyanuj Bora <while.alive.program@gmail.com>
2024-07-03 17:16:03 +0530

    incomplete

--- p69.c
+++ p69.c
@@ -13,13 +13,16 @@ void getLine(char[]);
 
 int main(void) {
 
-	char s[MAXLEN] = "", pattern[MAXLEN] = "";
+	char s[MAXLEN], pattern[MAXLEN];
 
 	printf("enter some string: ");
 	getLine(s);
 
+	printf("%ld\n", strlen(s));
+
 	printf("enter pattern: ");
 	getLine(pattern);
+	printf("%ld\n", strlen(pattern));
 
 	strindex(s, pattern);
 
@@ -29,8 +32,9 @@ int main(void) {
 void getLine(char s[]) {
 	int c, idx = 0;
 
-	while((c = getchar()) != 27)
+	while((c = getchar()) != EOF)
 		s[idx++] = c;
+	s[idx] = '\0';
 }
 
 void strindex(char s[], char pattern[]) {
@@ -40,9 +44,9 @@ void strindex(char s[], char pattern[]) {
 	for(int i = 0; s[i] != '\0'; i++) {
 		if(s[i] == pattern[0]) { // if any character in the string s matches the first character in the pattern
 			int count = 0;
-			for(int k = 0, j = i; s[j] == pattern[k] && s[j] != '\0'; k++, j++) // check if the rest of the characters in s match with pattern
-				count++; // basically counting the number of characters mathced with pattern
-			printf("%d\n%d\n", count, len);
+			for(int k = 0, j = i; (s[j] == pattern[k]) && s[j] != '\0'; k++, j++) // check if the rest of the characters in s match with pattern
+				count++; // basically counting the number of characters matched with pattern
+			printf("%d\n", count);
 			if(count == len) { // checking if the number of characters matched is same as the length of pattern
 				while(i > 0 && s[i] != '\n')
 					i--;
