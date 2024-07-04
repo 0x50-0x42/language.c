13924cd6ecbb9c87450f1cbfc6a6e57b55414660
Priyanuj Bora <while.alive.program@gmail.com>
2024-07-04 20:36:34 +0530

    K&R2 exercise 4.1

--- strrindex.c
+++ strrindex.c
@@ -8,7 +8,7 @@ int strrindex(char s[], char t[]) {
 	for(int i = strlen(s) - 1; i >= 0; i--) {
 		// compare each character of s with the last character of t
 		for(int k = strlen(t) - 1; s[i] == t[k] && i >= 0; k--, i--)
-			count++; // this loop will continue as long as s[j] == t[k] and j >= 0
+			count++; // this loop will continue as long as s[i] == t[k] and i >= 0
 
 		if(count == strlen(t))
 			return i;
