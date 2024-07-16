dac4b53ca22c361b370262f772d95cff17e99b16
Priyanuj Bora <while.alive.program@gmail.com>
2024-07-16 12:11:22 +0530

    K&R2 exercise 4.3 & exercise 4.4

--- stackOp.c
+++ stackOp.c
@@ -9,11 +9,6 @@ int top = -1;
 
 void push(float val) {
 
-	if(top == -1) {
-		puts("stack is empty!");
-		return;
-	}
-
 	if(top == MAX - 1 && stack[top] != 0.0f)
 		puts("stack overflow!");
 
@@ -38,4 +33,28 @@ float peek() {
 	}
 
 	return stack[top];
+}
+
+float duplicate() {
+	if(top == -1)
+		return FLT_MIN;
+	float val = stack[top];
+	stack[++top] = val; // duplicating the top element
+	return FLT_MAX;
+}
+
+float swap() {
+
+	if(top <= 0)
+		return FLT_MIN;
+
+	float temp = stack[top];
+	stack[top] = stack[top - 1];
+	stack[top - 1] = temp;
+
+	return FLT_MAX;
+}
+
+void clear() {
+	top = -1;
 }
