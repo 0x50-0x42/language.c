06ce0599b3bdddfc28d6d119d5e2adb211e40892
Priyanuj Bora <while.alive.program@gmail.com>
2024-07-17 15:22:08 +0530

    incomplete

--- p78.c
+++ p78.c
@@ -2,3 +2,19 @@
 	K&R2 exercise 4.5:
 		add access to library functions like sin, exp and pow;
 */
+
+#include<stdio.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<math.h>
+
+void push(float);
+float pop();
+float peek();
+float duplicate();
+float swap();
+void clear();
+
+int main(void) {
+	return 0;
+}
