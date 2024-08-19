24071a9e00d274ca781a0565ec3cb74b884189d4
0x50-0x42 <arya.grafter@gmail.com>
2024-08-19 12:05:35 +0530

    strcpy() using pointers

--- strcpy/strcpy.c
+++ strcpy/strcpy.c
@@ -4,6 +4,6 @@ void _strcpy(char *dest, char *src) {
 
 	int i = 0;
 
-	while((dest[i] = src[i]) != '\0') // first assign, then compare!!
+	while(dest[i] = src[i]) // '\0' is basically 0(false)
 		i++;
 }
