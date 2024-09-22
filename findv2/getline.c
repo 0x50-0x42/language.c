48da74e8cb644738ea1b7dd91d7cbaa3962beaab
0x50-0x42 <arya.grafter@gmail.com>
2024-09-22 12:51:05 +0530

    implementation of grep

--- findv2/getline.c
+++ findv2/getline.c
@@ -6,7 +6,7 @@ int _getline(char *s, int lim) {
 	char *base = s;
 	
 	while((c = getchar()) != EOF && (s - base) < lim)
-		*s++ = c == '\n' ? '\0' : c;
+		*s++ = c == '\n' ? '\0' : c; // instead of putting a newline, put a null character at the end
 	s--;
 
 	while(*s == ' ' || *s == '\t' || *s == '\n')
--- findv2/main.c
+++ findv2/main.c
@@ -7,23 +7,29 @@ int _getline(char*, int);
 
 int main(int argc, char *argv[]) {
 
-	int xclude = 0, line = 0;
-
 	if(argc == 1) {
 		puts("Usage: grep [-nx] [pattern]");
 		return 1;
 	}
 
-	char *found;
-
 	char string[SIZE];
+	
+	int xclude = 0, line = 0;
+
+	puts("enter some text:");
+
+	// take text input from user
+
 
 	int len = _getline(string, SIZE);
 
-	// check the options
-	int idx = 1;
+
+	// argument parsing!!
+	// evaluate the options/arguments given with the command
+
+	int idx = 1; // because argv + 0 will be the address of the pointer that stores the name of the program
 	while(*argv[idx] == '-') {
-		while(*(++argv[idx]) >= 97 && *(argv[idx]) <= 122) { // there could more than one character after the -
+		while(*(++argv[idx]) >= 97 && *argv[idx] <= 122) {
 			switch(*argv[idx]) {
 				case 'n':
 					line = 1;
@@ -32,46 +38,45 @@ int main(int argc, char *argv[]) {
 				case 'x':
 					xclude = 1;
 					break;
-					
+
 				default:
-					puts("invalid option");
-					return 1;
+					puts("invalid option!");
+					return 1; // stop processing
 			}
 		}
+
 		idx++;
 	}
 
 
+	// parse the input text for the pattern
+
 	int i = 0, countLine = 0, nonId = -1;
-	while(i != len) {
+
+	while(i < len) {
+
+		countLine++;
+
 		if(strstr(string + i, argv[idx]) != NULL) {
-			countLine++;
-			if(line) // if the line option is set
-				printf("%d ", countLine);
-			found = string + i;
 
-			if(!xclude) {
-				while(*found != '\0')
-					printf("%c", *found++);
-				printf("\n");
+			if(!xclude) { // if -x is not available
+
+				if(line) // if -n is available
+					printf("%d ", countLine);
+				printf("%s\n", string + i);
 			}
-
-			i = found - string;
-			nonId = ++i;
 		}
 
 		else {
-			i++;
-			if(string[i] == '\0') {
-				countLine++; // increment the line once the string reaches NULL character
-				if(xclude) {
-					if(nonId >= 0) {
-						printf("\n%d %s", countLine, string + nonId);
-						nonId = -1;
-					}
-				}
+			if(xclude) {
+				if(line)
+					printf("%d ", countLine);
+				printf("%s\n", string + i);
 			}
 		}
+
+		i += strlen(string + i); // always add to i the length of the current string
+		++i;
 	}
 
 	return 0;
