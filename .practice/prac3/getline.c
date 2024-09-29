4b10fa32e3863e9be047a0b9b95c59aa87a7f010
0x50-0x42 <arya.grafter@gmail.com>
2024-09-29 17:21:12 +0530

    incomplete

---
+++ .practice/prac3/getline.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int _getline(char* string, int lim) {
+
+	char *base = string;
+
+	while((c = getchar()) != EOF && (string - base) < lim)
+		*string++ = c;
+	string--;
+
+	while(*string == ' ' || *string == '\t' || *string == '\n')
+		string--;
+	*++string = '\0';
+
+	return (string - base);
+}
