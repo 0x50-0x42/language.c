a3a766c808be836d474cf811fe05665141267872
0x50-0x42 <arya.grafter@gmail.com>
2024-11-03 18:33:36 +0530

    removed

--- test/sortfields/def.h
+++
@@ -1,4 +0,0 @@
-#define MAXLINES 5000
-
-int readlines(char*(*)[3], int, int);
-void writelines(char*(*)[3], int, int);
--- test/sortfields/writelines.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-void writelines(char*(*line)[3], int numLines, int maxfield) {
-	for(int i = 0; i < numLines; i++) {
-		for(int j = 0; j < maxfield && *(line[i][j]) != '\0'; j++)
-			printf("%s ", line[i][j]);
-		printf("\n");
-	}
-}
