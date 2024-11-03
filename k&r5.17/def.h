1bf6aed8c4fe9834307f10b9db2bce34d182a88d
0x50-0x42 <arya.grafter@gmail.com>
2024-11-03 18:42:48 +0530

    incomplete

---
+++ k&r5.17/def.h
@@ -0,0 +1,6 @@
+#define MAXLINES 5000
+
+unsigned _getline(char*, int);
+
+unsigned readlines(char**, int);
+unsigned writelines(char**, int);
---
+++ k&r5.17/main.c
@@ -0,0 +1,18 @@
+// understood what I have to do from here: https://stackoverflow.com/a/6135979
+
+#include<stdio.h>
+#include<string.h>
+
+int main(int argc, char**argv) {
+
+	if(argc > 1) {
+		argc--;
+		argv++;
+	}
+
+	// naive argument handling
+	if(argc > 1) {
+	}
+
+	return 0;
+}
